c634b8c554ba286b34b4577608a4e60acf09f09f
AxelothLeohryn <alejandro97ss@gmail.com>
2023-10-22 14:57:37 +0200

    add/ latest score in results.html, navigation between home -> question -> results

--- script.js
+++ script.js
@@ -8,34 +8,34 @@ async function getQuestions() {
   );
   return await response.json().then((res) => res.results);
 }
-
 async function startGame() {
   i = 0;
   score = 0;
   questionsAndAnswers = await getQuestions();
   console.log(questionsAndAnswers);
   //pintar primera
-  await printQuestion(questionsAndAnswers);
+  printQuestion(questionsAndAnswers);
 }
-
-async function printQuestion(n) {
-  let answers = [
-    n[i].incorrect_answers[0],
-    n[i].incorrect_answers[1],
-    n[i].incorrect_answers[2],
-    n[i].correct_answer,
-  ];
-
-  answers.sort(() => Math.random() - 0.5);
-  console.log(answers);
-  document.getElementById(
-    "preguntas-y-respuestas"
-  ).innerHTML = ` <h3>${n[i].question}</h3>
-                  <button class="answer-button" type="button">${answers[0]}</button>
-                  <button class="answer-button" type="button">${answers[1]}</button>
-                  <button class="answer-button" type="button">${answers[2]}</button>
-                  <button class="answer-button" type="button">${answers[3]}</button>`;
-  checkAnswers();
+function printQuestion(n) {
+  if (questionsAndAnswers && questionsAndAnswers[i]) {
+    let answers = [
+      n[i].incorrect_answers[0],
+      n[i].incorrect_answers[1],
+      n[i].incorrect_answers[2],
+      n[i].correct_answer,
+    ];
+  
+    answers.sort(() => Math.random() - 0.5);
+    console.log(answers);
+    document.getElementById(
+      "preguntas-y-respuestas"
+    ).innerHTML = ` <h3>${i + 1}. ${n[i].question}</h3>
+                    <button class="answer-button" type="button">${answers[0]}</button>
+                    <button class="answer-button" type="button">${answers[1]}</button>
+                    <button class="answer-button" type="button">${answers[2]}</button>
+                    <button class="answer-button" type="button">${answers[3]}</button>`;
+    checkAnswers();
+  }
 }
 function checkAnswers() {
   let botonesRespuestas = document.getElementsByClassName("answer-button");
@@ -57,15 +57,12 @@ function checkAnswers() {
     });
   }
 }
-
 function cancelButtons() {
   let botonesRespuestas = document.getElementsByClassName("answer-button");
   for (let button of botonesRespuestas) {
     button.disabled = true;
   }
 }
-
-//funcion que consigue la fecha actual
 function getCurrentDate() {
   const currentDate = new Date();
   const year = currentDate.getFullYear();
@@ -77,31 +74,60 @@ function getCurrentDate() {
   const date = `${day}/${month}/${year} - ${hours}:${minutes}:${seconds}`;
   return date;
 }
-
-//funcion que va a añadir el resultado en local storage <----LLAMAR A FUNCION EN ULTIMO BOTON DE NEXT
 function storeResultsLocal() {
   // Obtener los resultados existentes desde el localstorage, si los hay
   let existingResults = JSON.parse(localStorage.getItem("Results")) || [];
-
   // Agregar el nuevo resultado
   existingResults.push({
     score: score,
     date: getCurrentDate(),
   });
-
   // Guardar en local storage
   localStorage.setItem("Results", JSON.stringify(existingResults));
 }
-// storeResultsLocal(score)
-// function checkAnswers() {}
+function showScore() {
+  let resultado = document.getElementById('resultado'); //Obtengo la lista de scores guardada en local storage
+  let scores = JSON.parse(localStorage.getItem("Results")).reverse(); //Le doy la vuelta para acceder al ultimo resultado siempre
+  console.log(scores);
+    resultado.innerHTML = scores[0].score;
+}
 
-startGame();
+//Empezar startGame() solo si estamos en question.html
+if (window.location.pathname.includes("question.html")) {
+  startGame();
+}
+//Empezar showScore() solo si estamos en results.html
+if (window.location.pathname.includes("results.html")) {
+  showScore();
+}
+
 
 //Eventos
-document.getElementById("next-button").addEventListener("click", (event) => {
-  event.preventDefault();
-  i++;
-  printQuestion(questionsAndAnswers);
-});
+
+//Boton Start
+const startButton = document.querySelector('#start-button');
+if (startButton) {
+  startButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    // startGame();  Ya se ejecuta al cambiar de html
+    window.location.href = 'question.html';
+  })
+}
+//Boton Next
+const nextButton = document.getElementById("next-button");
+if (nextButton) {
+  nextButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    if (i < 9) {
+      i++;
+      printQuestion(questionsAndAnswers);
+    }
+    else if (i >= 9) {
+      storeResultsLocal();
+      window.location.href='results.html'
+    }
+  });
+}
+
 
 
